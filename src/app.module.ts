75dfff8986fd80c3f8b3cf2da8dc600e96a7c82c
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-05-23 21:24:24 +0700

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/you_app'),
+  ],
 })
 export class AppModule {}
