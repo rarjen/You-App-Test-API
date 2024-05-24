d5746a9cae5049d08d6f13d1db45e61d2f0847b7
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-05-24 10:02:54 +0700

    update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,10 +5,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema, User } from 'schema/user.schema';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Profile, ProfileSchema } from 'schema/profile.schema';
+import { Horroscopehelper } from 'src/helpers/horroscope.helpers';
+import { ConversionHelper } from 'src/helpers/conversion.helpers';
+// import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Profile.name, schema: ProfileSchema },
+    ]),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
@@ -22,6 +29,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     }),
   ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, Horroscopehelper, ConversionHelper],
 })
 export class UsersModule {}
