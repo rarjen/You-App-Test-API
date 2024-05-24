5b19864c2cefc377d0361c1bd59328d9e606fbea
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-05-24 18:53:47 +0700

    update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,13 +9,14 @@ import { InjectModel } from '@nestjs/mongoose';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '../../schema/user.schema';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { LoginUserDTO } from './dto/login.user.dto';
 import { CreateProfileDTO } from './dto/create.profile.user.dto';
 import { Profile } from 'schema/profile.schema';
 import { Horroscopehelper } from '../helpers/horroscope.helpers';
 import { ConversionHelper } from 'src/helpers/conversion.helpers';
 import { UpdateProfileDTO } from './dto/update.profile.user.dto';
+import { profile } from 'console';
 
 @Injectable()
 export class UsersService {
@@ -73,17 +74,31 @@ export class UsersService {
   }
 
   async createProfileUser(
+    userId: string,
     createProfileUserDTO: CreateProfileDTO,
   ): Promise<Profile> {
     const { display_name, gender, birthday, weight, height } =
       createProfileUserDTO;
 
+    const user = await this.userModel.findById(userId);
+    if (!user) {
+      throw new NotFoundException(`User with ID "${userId}" not found`);
+    }
+
+    const profileExist = await this.profileModel.findOne({ user_id: userId });
+    if (profileExist) {
+      throw new BadRequestException(
+        `User with ID "${userId} already have a profile"`,
+      );
+    }
+
     const birthdayConvertToDate = new Date(birthday);
     const heightConverstionToFeet =
       this.conversionHelper.convertCmToFeet(height);
 
-    const result = new this.profileModel({
+    const newProfile = new this.profileModel({
       _id: uuidv4(),
+      user_id: user._id,
       display_name,
       gender,
       birthday,
@@ -99,11 +114,14 @@ export class UsersService {
       heightInCentimenters: `${height} Cm`,
       heightInFeet: `${heightConverstionToFeet} Feet`,
     });
-    return result.save();
+
+    const profile = await newProfile.save();
+
+    return profile;
   }
 
   async updateProfileUser(
-    id: string,
+    userId: string,
     updateProfileDTO: UpdateProfileDTO,
   ): Promise<Profile> {
     const { display_name, gender, birthday, weight, height } = updateProfileDTO;
@@ -112,8 +130,8 @@ export class UsersService {
     const heightConverstionToFeet =
       this.conversionHelper.convertCmToFeet(height);
 
-    const result = await this.profileModel.findByIdAndUpdate(
-      id,
+    const result = await this.profileModel.findOneAndUpdate(
+      { user_id: userId },
       {
         display_name,
         gender,
@@ -134,8 +152,14 @@ export class UsersService {
     );
 
     if (!result) {
-      throw new NotFoundException(`Profile with ID "${id}" not found`);
+      throw new NotFoundException(`Profile with user ID "${userId}" not found`);
     }
+
+    return result;
+  }
+
+  async getProfileUser(id: string): Promise<User> {
+    const result = await this.userModel.findById(id);
 
     return result;
   }
