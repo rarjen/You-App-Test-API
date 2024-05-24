ac28ef3942a78ba81542af94e6166db036a350f4
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-05-24 10:03:02 +0700

    feat: create create.profile.user.dto.ts

---
+++ src/users/dto/create.profile.user.dto.ts
@@ -0,0 +1,25 @@
+import { IsDateString, IsEmail, IsIn, IsNotEmpty } from 'class-validator';
+
+export class CreateProfileDTO {
+  display_name: string;
+
+  @IsIn(['Male', 'Female'])
+  gender: string;
+
+  @IsDateString()
+  birthday: string;
+
+  horoscope?: string;
+
+  zodiac?: string;
+
+  height: number;
+
+  heightInCentimeter: string;
+
+  heightInInches: string;
+
+  weight: number;
+
+  weightInKilograms: string;
+}
