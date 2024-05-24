c869bc4de4a0fd91acb0a03739ec45fcc312bc43
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-05-24 10:02:44 +0700

    feat: create horroscope.helpers.ts

---
+++ src/helpers/horroscope.helpers.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class Horroscopehelper {
+  getChineseZodiac(year: number): string {
+    const zodiacs = [
+      'Monkey',
+      'Rooster',
+      'Dog',
+      'Pig',
+      'Rat',
+      'Ox',
+      'Tiger',
+      'Rabbit',
+      'Dragon',
+      'Snake',
+      'Horse',
+      'Goat',
+    ];
+    return zodiacs[year % 12];
+  }
+
+  getWesternHoroscope(date: Date): string {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+
+    if ((month == 1 && day >= 20) || (month == 2 && day <= 18)) {
+      return 'Aquarius';
+    } else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+      return 'Pisces';
+    } else if ((month == 3 && day >= 21) || (month == 4 && day <= 19)) {
+      return 'Aries';
+    } else if ((month == 4 && day >= 20) || (month == 5 && day <= 20)) {
+      return 'Taurus';
+    } else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+      return 'Gemini';
+    } else if ((month == 6 && day >= 21) || (month == 7 && day <= 22)) {
+      return 'Cancer';
+    } else if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) {
+      return 'Leo';
+    } else if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) {
+      return 'Virgo';
+    } else if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) {
+      return 'Libra';
+    } else if ((month == 10 && day >= 23) || (month == 11 && day <= 21)) {
+      return 'Scorpio';
+    } else if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) {
+      return 'Sagittarius';
+    } else if ((month == 12 && day >= 22) || (month == 1 && day <= 19)) {
+      return 'Capricorn';
+    }
+  }
+}
