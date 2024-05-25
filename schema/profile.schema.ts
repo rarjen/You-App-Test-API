0eaf40d6b0f3222ae316053709e07e37ce5e1812
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-05-25 09:32:33 +0700

    update profile.schema.ts

--- schema/profile.schema.ts
+++ schema/profile.schema.ts
@@ -1,6 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Schema as MongooseSchema, Types } from 'mongoose';
-import { User } from './user.schema';
 
 enum Gender {
   MALE = 'Male',
