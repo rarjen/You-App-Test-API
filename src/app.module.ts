b8e24c23512f3857653146782bc793ae0acd463e
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-05-25 09:32:37 +0700

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,18 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 // import { JwtStrategyModule } from './jwt-strategy/jwt-strategy.module';
+import { MessageService } from './message/message.service';
+import { MessageModule } from './message/message.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './auth/jwt.strategy';
 
 @Module({
   imports: [
     UsersModule,
+    MessageModule,
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/you_app'),
+    MessageModule,
   ],
 })
 export class AppModule {}
